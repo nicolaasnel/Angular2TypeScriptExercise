5dcd58df1fcf5bae9ae2f0270c18d83493baf352
Nicolaas  Nel <nicolaas.cm.nel@gmail.com>
2016-03-24 07:37:28 +0200

    Clean up favourite code

--- app/favourite.component.ts
+++ app/favourite.component.ts
@@ -3,15 +3,18 @@ import {Component} from 'angular2/core'
 @Component({
   selector: "fav",
   template: `
-    <span [class]="favClass" (click)="onClick()"></span>  
+    <span 
+      class="glyphicon"
+      [class.glyphicon-star-empty] = "!selected"
+      [class.glyphicon-star] = "selected"
+      (click)="onClick()">
+     </span>  
   `
 })
 
 export class FavComponent{
   selected = false
-  favClass = "glyphicon glyphicon-star-empty"
   onClick() {
     this.selected = !this.selected
-    this.favClass = this.selected ? "glyphicon glyphicon-star" : "glyphicon glyphicon-star-empty"
   }
 }
